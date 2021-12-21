d85d2a4e2982807de2e05c94bfc1e68a63f39dda
Dimanok1989 <kolgaev@gmail.com>
2021-12-21 18:20:53 +0300

    Смена маршрутизации для применение стандартных посредников Laravel

--- src/Providers/SendrequestServiceProvider.php
+++ src/Providers/SendrequestServiceProvider.php
@@ -23,7 +23,7 @@ class SendrequestServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__ . '/../../routes/sendrequest.php');
+        $this->loadRoutesFrom(__DIR__ . '/../../routes/web.php');
 
         $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
     }
