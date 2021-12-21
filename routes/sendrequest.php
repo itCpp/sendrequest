8bbfd4a734cec822ac1d67d9d4794e17b27dfb14
Dimanok1989 <kolgaev@gmail.com>
2021-12-21 17:47:24 +0300

    Переопределение маршрута

--- routes/sendrequest.php
+++ routes/sendrequest.php
@@ -2,4 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/sendRequest', '\ItCpp\Sendrequest\SendRequest@send');
+Route::post('/itcpp/sendrequest', '\ItCpp\Sendrequest\SendRequest@send');
