050aa4402ca828a2efc7f75734f1d0c4f8da06ad
Dimanok1989 <kolgaev@gmail.com>
2021-12-21 18:27:18 +0300

    Посредник для маршрутизации

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::post('/itcpp/sendrequest', '\ItCpp\Sendrequest\SendRequest@send');
+Route::post('/itcpp/sendrequest', '\ItCpp\Sendrequest\SendRequest@send')->middleware('web');
